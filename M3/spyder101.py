89f0e9e6d214e185da248604683b337d4fae2370
jrb28 <james.bradley@mason.wm.edu>
2023-08-16 15:48:46 -0400

    Add files via upload

---
+++ M3/spyder101.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def check_type(x, y):
+    return type(x) == type(y)
+
+
+print('Hello World!')
+x = 1
+
+
+print(check_type(1.,2))
+print(check_type(1,2))
